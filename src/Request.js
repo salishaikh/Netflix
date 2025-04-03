74cbf8911000e5320ef83a98753228a3e7cc2e47
salishaikh <88184408+salishaikh@users.noreply.github.com>
2025-04-03 10:31:36 +1030

    Update Request.js

--- src/Request.js
+++ src/Request.js
@@ -4,7 +4,7 @@ const request = {
     requestPopular : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated : `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending : `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}`,
-    requestplaying : `https://api.themoviedb.org/3/now_playing/movie/day?api_key=${key}`,
+
     requestlist : `
     https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&page=10&with_genres=35?&with_genre=2`,
     
